8dec13163917c2d2569da90d8dfa9f858cc8b3d5
Hung li yu <105021025@live.asia.edu.tw>
2018-01-02 13:03:58 +0800

    up

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+    @FXML
+    ImageView img99;
+
+    @FXML
+    public int[] getLocation(){
+        int[] location = {0,0};
+        location[0] = ((int)img99.getLayoutX()/50)+1;
+        location[1] = ((int)img99.getLayoutY()/50);
+        System.out.print(location[0] + " " + location[1]);
+        return location;
+    }
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,10 +16,37 @@ public class Main extends Application {
         primaryStage.setTitle("自動循路模擬");
         primaryStage.setScene(new Scene(root, 490, 500));
         primaryStage.show();
+        int[][] map =  {{0,0,0,0,0,0,0,0,0,0},
+                        {1,0,1,1,1,0,1,1,1,0},
+                        {0,0,1,0,0,0,1,0,0,0},
+                        {0,1,1,0,1,1,1,1,0,1},
+                        {0,0,0,0,1,0,0,0,0,0},
+                        {1,1,1,0,1,0,1,1,1,0},
+                        {0,0,0,0,0,0,1,0,0,0},
+                        {0,1,1,0,1,0,1,0,1,1},
+                        {0,1,1,0,1,0,1,0,0,0},
+                        {0,0,0,0,1,0,0,0,1,0}};
+        printMap(map);
+        root.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println(event.getScreenX());
+                System.out.println(event.getScreenY());
+            }
+        });
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void printMap(int[][] map){
+        for(int i = 0;i<map.length;i++){
+            for(int k = 0;k<map.length;k++){
+                System.out.print(map[i][k] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
